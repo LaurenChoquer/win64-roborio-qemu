31705dcc1a3d8951fbb46be70d6abdcc43ac407b
Charlie Hall <CobaltCake@gmail.com>
2016-07-26 15:47:24 -0700

    updated kernel

--- stock.bat
+++ stock.bat
@@ -1,17 +1,18 @@
 @echo off
 
 echo Downloading Xilinx Zynq A9 Linux Kernel...
-cscript //nologo download.vbs http://www.wiki.xilinx.com/file/view/2014.3-release.tar.xz/526959742/2014.3-release.tar.xz
+cscript //nologo download.vbs http://www.wiki.xilinx.com/file/view/2016.2-zed-release.tar.xz/586243277/2016.2-zed-release.tar.xz
 echo Done.
 echo Extracting Kernel...
-"C:\Program Files\7-Zip\7z.exe" x 2014.3-release.tar.xz
-"C:\Program Files\7-Zip\7z.exe" x -o2014.3-release 2014.3-release.tar
+"C:\Program Files\7-Zip\7z.exe" x 2016.2-zed-release.tar.xz
+"C:\Program Files\7-Zip\7z.exe" x -o2016.2-zed-release 2016.2-zed-release.tar
 echo Done.
+pause
 echo Getting kernel files...
 mkdir linux
-move 2014.3-release\zed\devicetree.dtb linux/devicetree.dtb
-move 2014.3-release\uImage linux/uImage
-rmdir /q /s 2014.3-release
-del 2014.3-release.tar
-del 2014.3-release.tar.xz
+move 2016.2-zed-release\zed\devicetree.dtb linux/devicetree.dtb
+move 2016.2-zed-release\zed\uImage linux/uImage
+rmdir /q /s 2016.2-zed-release
+del 2016.2-zed-release.tar
+del 2016.2-zed-release.tar.xz
 echo Done.
